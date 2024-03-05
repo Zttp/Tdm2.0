5cb6e1cf0d69f8a29b47931e343dc18a7829abaf
Zttp <162319093+Zttp@users.noreply.github.com>
2024-03-05 17:22:23 +0300

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -3,12 +3,16 @@ import { Teams } from 'pixel_combats/room';
 
 export const RED_TEAM_NAME = "Red";
 export const BLUE_TEAM_NAME = "Blue";
-export const RED_TEAM_DISPLAY_NAME = "PS";
-export const BLUE_TEAM_DISPLAY_NAME = "ZP";
+export const GREEN_TEAM_NAME = "Green";
+export const RED_TEAM_DISPLAY_NAME = "Red";
+export const BLUE_TEAM_DISPLAY_NAME = "Blue";
+export const GREEN_TEAM_DISPLAY_NAME = "Green";
 export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
 export const RED_TEAM_SPAWN_POINTS_GROUP = 2;
+export const GREEN_TEAM_SPAWN_POINTS_GROUP = 1;
 export const BLUE_TEAM_COLOR = new Color(0, 3, 7, 0);
 export const RED_TEAM_COLOR = new Color(1, 8, 9, 0);
+export const GREEN_TEAM_COLOR = new Color(4, 1, 2, 0);
 
 export function create_team_blue() {
     Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, BLUE_TEAM_COLOR);
@@ -23,3 +27,9 @@ export function create_team_red() {
     team.Spawns.SpawnPointsGroups.Add(RED_TEAM_SPAWN_POINTS_GROUP);
     return team;
 }
+
+export function create_team_green() {
+    Teams.Add(GREEN_TEAM_NAME, GREEN_TEAM_DISPLAY_NAME, GREEN_TEAM_COLOR);
+    const team = Teams.Get(GREEN_TEAM_NAME);
+    team.Spawns.SpawnPointsGroups.Add(GREEN_TEAM_SPAWN_POINTS_GROUP);
+    return team;
